f08bafb1120e7734054344149b692afdfdd56888
Nohdajeong <dajeong0404@naver.com>
2022-11-11 11:20:04 +0900

    수정
    - 리소스 이동
    - 발표 ppt 제작
    - title state 수정 (오브젝트 배치)

--- mygame_test.py
+++ mygame_test.py
@@ -5,6 +5,6 @@ import title_state
 import play_state
 
 pico2d.open_canvas()
-#game_framework.run(title_state)
-game_framework.run(play_state)
+game_framework.run(title_state)
+# game_framework.run(play_state)
 pico2d.close_canvas()
--- title_state.py
+++ title_state.py
@@ -5,33 +5,50 @@ import play_state
 
 background = None
 character = None
+quiz = None
+store = None
+
+sx, sy = 400, 300
+x, y = sx, sy
+ax, ay = x, y
 
 def enter():
-    global background, character
+    global background, character, quiz, store
     background = load_image('School_main.png')
     character = load_image('run_character.png')
+    quiz = load_image('quiz.png')
+    store = load_image('store.png')
 
 def exit():
-    global background, character
+    global background, character, quiz, store
     del character
     del background
+    del quiz
+    del store
 
 def update():
     pass
 
 def handle_events():
+    global x, y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
             game_framework.change_state(play_state)
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            x, y = event.x, event.y
+
+
     delay(0.01)
 
 def draw():
     clear_canvas()
     background.draw(400, 220)
     character.draw(400, 300)
+    quiz.draw(700, 500)
+    store.draw(700, 400)
     update_canvas()
 
 
